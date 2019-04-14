c729759be3ebe09578f1bb6277d68723cde70176
Jakub Brehuv <kubek.breha@gmail.com>
2019-04-14 23:54:30 +0200

    new game button

---
+++ SoClose/Assets/Scripts/ResultHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class ResultHandler : MonoBehaviour
+{
+
+    public Button newGameButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newGameButton.onClick.AddListener(ReloadScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReloadScene()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+
+}
